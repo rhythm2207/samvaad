06d0c5aee375ad822114fbe01862069d5cfbaf48
Rhythm Singh <rhysun08@gmail.com>
2022-09-19 03:29:15 +0530

    error

---
+++ config/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GITHUB_CLIENT_ID: '456b88bc0cb887c4e551',
+  GITHUB_CLIENT_SECRET: 'a7216f623eabd4cd38435f681703057fc8d4f037',
+  GITHUB_CALLBACK_URL: 'https://localhost:8080/api/user/authViaGitHub/callback',
+  DBURL: 'mongodb://localhost:27017/reforum',
+};
